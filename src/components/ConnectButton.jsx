7b3b3dda6a98692f53ae4c884119e72f2dbf27cd
sundayonah <sundayonah94@gmail.com>
2023-07-30 12:36:12 +0100

    add connect button

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import PageButton  from './PageButton'
 
-const ConnectButton = () => {
+const ConnectButton = ({isConnected, signerAddress, getSigner, provider}) => {
+    const displayAddress = `${signerAddress?.substring(0,5)}...${signerAddress?.substring(35,39)}`
   return (
-    <div>ConnectButton</div>
+    <>
+    {isConnected() ? (
+        <div className='buttonContainer'>
+           <PageButton name={displayAddress} />
+        </div>
+    ) : (
+         <div className='btn my-2 connectButton'
+         onClick={() => getSigner(provider)}
+         >
+            Connect Wallet
+         </div>
+    )}
+    </>
   )
 }
-
 export default ConnectButton
