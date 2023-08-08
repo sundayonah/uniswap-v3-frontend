3cce81c221b66b3cf83ca3241662e9ea0e101df8
sundayonah <sundayonah94@gmail.com>
2023-08-08 18:42:29 +0100

    fix:TokenSelectedModal

--- src/components/TokenSelectModal.jsx
+++ src/components/TokenSelectModal.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import "../styles/TokenSelectModal.css"
+// import
 
 const TokenSelectModal = (props) => {
 
     const tokens = [
-        { id: 1, title: "WrappedEther", body: "WETH", address: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6" },
-        { id: 2, title: "UNISWAP", body: "UNI", address: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984" },
-        { id: 2, title: "UNISWAP", body: "UNI", address: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984" }
+        { id: 1, icon: "/assets/wrapEther.png", title: "Wrapped Ether", symbol: "WETH", address: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6" },
+        { id: 2, icon: "/assets/uniswap.png", title: "Uniswap", symbol: "UNI", address: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984" },
+    ]
+
+    const setTokens = [
+        { id: 1, icon: "/assets/wrapEther.png", symbol: "WETH" },
+        { id: 2, icon: "/assets/uniswap.png", symbol: "UNI" },
     ]
 
     return (
@@ -24,23 +29,47 @@ const TokenSelectModal = (props) => {
                         placeholder="Search name or paste address..."
                     />
                 </div>
+                <div className="selectedToken">
+                  {setTokens.map(setToken => 
+                    <div key={setToken.id}
+                    className="setToken">
+                      <img
+                        className="tokensIcon"
+                        src={setToken.icon}
+                        width='25%' 
+                        height='100%'
+                        alt="" />
+                        <div>
+                            {setToken.symbol}
+                        </div>
+                    </div>
+                  )}
+                </div>
+                <hr />
                 <div className="tokenList"
                     onClick={props.onClose}>
                     {tokens.map(token =>
                         <div key={token.id}
                             className="tokenNames"
                             onClick={() => {
-                                props.setTokenName(token.body);
+                                props.setTokenName(token.symbol);
                                 props.setToken(token);
-                            }
-                            }
-                        >
-                            <div className="shortTokenName">
+                            }}
+                         >        
+                            <img
+                            className="tokensIcon"
+                            src={token.icon}
+                            width='15%' 
+                            height='100%'
+                            alt="" />
+                         <div>
+                            <div className="tokensTitle">
                                 {token.title}
                             </div>
-                            <div className="fullTokenName">
-                                {token.body}
+                            <div className="tokensSymbol">
+                                {token.symbol}
                             </div>
+                         </div>
                         </div>
                     )}
                 </div>
